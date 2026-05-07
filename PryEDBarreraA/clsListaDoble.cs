2f00aac1de678240dd220d78ae4a05d63c8b4d0b
agustinbarrera99 <agustinbarrera.ri@gmail.com>
2026-05-07 11:34:15 -0300

    Se agrego herencia a todas las clases de estructuras lineales

--- PryEDBarreraA/clsListaDoble.cs
+++ PryEDBarreraA/clsListaDoble.cs
@@ -10,17 +10,10 @@ using System.Windows.Forms;
 
 namespace PryEDBarreraA
 {
-    internal class clsListaDoble
+    internal class clsListaDoble : clsEstructurasLineales
     {
-        private clsNodo pri;
         private clsNodo ult;
-        private string NombreArchivo;
 
-        public clsNodo Primero
-        {
-            get { return pri; }
-            set { pri = value; }
-        }
         public clsNodo Ultimo
         {
             get { return ult; }
@@ -67,52 +60,6 @@ namespace PryEDBarreraA
                     }
                 }
             }
-        }
-        public void Recorrer(DataGridView dgv)
-        {
-            clsNodo aux = Primero;
-            dgv.Rows.Clear();
-            while (aux != null)
-            {
-                dgv.Rows.Add(aux.Codigo, aux.Nombre, aux.Tramite);
-                aux = aux.Siguiente;
-            }
-
-        }
-        public void Recorrer(ListBox lst)
-        {
-            clsNodo aux = Primero;
-            lst.Items.Clear();
-            while (aux != null)
-            {
-                lst.Items.Add(aux.Codigo);
-                aux = aux.Siguiente;
-            }
-        }
-        public void Recorrer(ComboBox cmb)
-        {
-            clsNodo aux = Primero;
-            cmb.Items.Clear();
-            while (aux != null)
-            {
-                cmb.Items.Add(aux.Codigo + " - " + aux.Nombre + " - " + aux.Tramite);
-                aux = aux.Siguiente;
-            }
-        }
-        public void Recorrer()
-        {
-            StreamWriter AD = new StreamWriter(NombreArchivo, true);
-            clsNodo aux = Primero;
-            while (aux != null)
-            {
-                AD.Write(aux.Codigo);
-                AD.Write(";");
-                AD.Write(aux.Nombre);
-                AD.Write(";");
-                AD.WriteLine(aux.Tramite);
-                aux = aux.Siguiente;
-            }
-            AD.Close();
         }
         public void Eliminar(int cod)
         {
--- PryEDBarreraA/clsPila.cs
+++ PryEDBarreraA/clsPila.cs
@@ -8,18 +8,8 @@ using System.Windows.Forms;
 
 namespace PryEDBarreraA
 {
-    internal class clsPila
+    internal class clsPila : clsEstructurasLineales
     {
-        private clsNodo pri;
-        public string NombreArchivo;
-
-        public clsNodo Primero
-        {
-            get { return pri; }
-            set { pri = value; }
-        }
-
-        //Metodos de la clase: agregar, eliminar y recorrer
 
         public void Agregar(clsNodo nuevo)
         {
@@ -39,53 +29,6 @@ namespace PryEDBarreraA
             {
                 pri = pri.Siguiente;
             }
-        }
-
-        public void Recorrer(DataGridView dgv)
-        {
-            clsNodo aux = Primero;
-            dgv.Rows.Clear();
-            while (aux != null)
-            {
-                dgv.Rows.Add(aux.Codigo, aux.Nombre, aux.Tramite);
-                aux = aux.Siguiente;
-            }
-
-        }
-        public void Recorrer(ListBox lst)
-        {
-            clsNodo aux = Primero;
-            lst.Items.Clear();
-            while (aux != null)
-            {
-                lst.Items.Add(aux.Codigo + " - " + aux.Nombre + " - " + aux.Tramite);
-                aux = aux.Siguiente;
-            }
-        }
-        public void Recorrer(ComboBox cmb)
-        {
-            clsNodo aux = Primero;
-            cmb.Items.Clear();
-            while (aux != null)
-            {
-                cmb.Items.Add(aux.Codigo + " - " + aux.Nombre + " - " + aux.Tramite);
-                aux = aux.Siguiente;
-            }
-        }
-        public void Recorrer()
-        {
-            StreamWriter AD = new StreamWriter(NombreArchivo, true);
-            clsNodo aux = Primero;
-            while (aux != null)
-            {
-                AD.Write(aux.Codigo);
-                AD.Write(";");
-                AD.Write(aux.Nombre);
-                AD.Write(";");
-                AD.WriteLine(aux.Tramite);
-                aux = aux.Siguiente;
-            }
-            AD.Close();
         }
     }
 }
